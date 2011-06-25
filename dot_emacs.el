e15e048e7efaa79e339c46f7489719b325c45cc5
Konstantin Levinski <konstantin.levinski@gmail.com>
2011-06-26 01:28:29 +0800

    Sync functionality.

--- dot_emacs.el
+++ dot_emacs.el
@@ -14,3 +14,22 @@
 (setq 
  browse-url-browser-function 'browse-url-generic 
  browse-url-generic-program "chromium-browser") 
+
+;; Github-specific part:
+
+;; Github sync. 
+(setq dot-emacs-at-github  "http://korantu.github.com/emacs/dot_emacs.el")
+(defun kdl-sync () 
+  "Pull .emacs from github http page."
+  (interactive)
+  ( let
+      ((script (concat "wget -O ~/.emacs " dot-emacs-at-github)))
+  (shell-command script)))
+
+;; This is how functions are done.
+(defun kdl-test ()
+  "Defun test"
+  (interactive)
+  (message "hi"))
+
+
